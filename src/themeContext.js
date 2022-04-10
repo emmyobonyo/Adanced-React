ba80014a78a5958f70f48bcef206d82e65c12f60
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-04-10 15:56:27 +0300

    Create toggle button

--- src/themeContext.js
+++ src/themeContext.js
@@ -3,10 +3,22 @@ import React, {Component} from 'react'
 const { Provider, Consumer } = React.createContext();
 
 class ThemeContextProvider extends Component {
+    state = {
+        theme: 'light'
+    }
+
+    toggleTheme = () => {
+        this.setState(prevState => {
+            return {
+                theme: prevState.theme === 'light' ? 'dark' : 'light',
+            }
+        })
+    }
     render() {
         return (
-            <Provider value='dark'>
+            <Provider value={this.state.theme}>
                 {this.props.children}
+                <button onClick={this.toggleTheme}>Change Theme</button>
             </Provider>
         )
     }    
